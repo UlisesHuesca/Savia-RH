7124ad13dc575f69c45fe037941a6ae1962eb48b
olmos-dev <albrhto94@gmail.com>
2023-11-16 17:58:23 -0600

    crear app esquema

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'dashboard.apps.DashboardConfig',
     'proyecto.apps.ProyectoConfig',
     'user.apps.UserConfig',
+    'esquema',
 
 # Extensions - installed with pip3 / requirements.txt
     'django_extensions',
@@ -137,7 +138,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Mexico_City'
 
